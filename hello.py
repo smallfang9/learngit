b8c1b041b6d4cd6a724187c23d27e06a2df071a2
smallfang9 <991541031@qq.com>
2021-09-14 18:41:54 +0800

    init hello

---
+++ hello.py
@@ -0,0 +1,7 @@
+Merge branch 'master' of github.com:smallfang9/learngit
+
+#Pleaase enter a commit messsage to explain why this merge is necessary,
+#especially if it merges an updated upstream into a topic branch.
+#
+#Lines starting with '#' will be ignore, and an empty message aborts
+#the commit.
